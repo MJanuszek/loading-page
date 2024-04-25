afbfd8e45b63fcda4f7dd51607a94c0f39391601
Monika Januszek <monika.januszek.mail@gmail.com>
2024-04-25 13:33:35 +0200

    show welcome page after loading is completed

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const loadingTitle = document.getElementById("loading-nb");
 const loadingBar = document.getElementById("loading-progress");
+const page = document.getElementById("container-div");
 
 let number = 0;
+let loaded = false;
+
+function loadedAnimation() {
+  if (loaded === true) {
+    page.style.backgroundImage = 'url("/scott-webb-oRWRlTgBrPo-unsplash.jpg")';
+    loadingTitle.textContent = "WELCOME";
+    loadingTitle.classList.add("welcome");
+    loadingBar.style.width = 0;
+  } else {
+    return;
+  }
+}
 
 function loagindPage() {
   let loading = setInterval(() => {
@@ -10,10 +23,13 @@ function loagindPage() {
     loadingBar.style.width = number / 5 + "%";
 
     if (number >= 100) {
+      loaded = true;
+      console.log(loaded);
+      loadedAnimation();
       clearInterval(loading);
       return;
     }
-  }, 100);
+  }, 50);
 }
 
 loagindPage();
